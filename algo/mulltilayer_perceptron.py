cee156925dccff427dea290e17adfcd33ea05e26
botsk <usaikiran89@gmail.com>
2019-08-17 16:43:50 +0530

    mlp modified

--- algo/mulltilayer_perceptron.py
+++ algo/mulltilayer_perceptron.py
@@ -6,55 +6,66 @@ from random import random
 import math
 
 
-EPOCHS = 500
-LR = 2
+EPOCHS = 20000
+LR = 0.9
+BIAS = -1
 
 activate = np.vectorize(lambda x: 1/( 1+math.exp(-x) ) )
-#activate = np.vectorize( lambda x : 1 if x>0.5 else 0 )
+square = np.vectorize( lambda x: x*x )
 
+round = lambda x,n : int(x*(10**n)) / (10**n)
 
 def predict( I, H ):
 
 	A = []
 	input = I
 	for h in H:
-		A.append( activate( np.dot(input, h) ) )
+		A.append( activate( np.dot(input, h)+BIAS ) )
 		input = A[-1]
 
 	return A
 
 
+def train( I, T, W ):
+
+	for epoch in range(EPOCHS):
+
+		A = predict(I, W)
+		e = T-A[-1]
+
+		verror = round(np.sum( square(e) ), 3)
+		print "epoch", epoch, " validation error :", verror
+		if verror<0.001:
+			print "early stopping"
+			break
+
+		for x in range(1, len(W)+1):
+
+			i = A[-(x+1)] if x<len(W) else I
+			a = A[-x]
+			w = W[-x]
+			e = e*(1-a)*a
+
+			W[-x] += LR*np.dot( np.transpose(i), e )
+			e = np.dot( e, np.transpose(w) )
+	
+	return W
+
+
 if __name__ == "__main__":
 
 	I = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-	T = np.array([[0], [0], [0], [1]])
+	T = np.array([[1], [0], [0], [1]])
 
-	H = []
-	H.append( np.array( [[(random()-0.5)*0.2 for x in range(1)]]*I.shape[1] ) )
-	#print H
+	layers = [3, 3]
+	layers.append(T.shape[1])
+	W = []
 
-	for _ in range(EPOCHS):
+	inputNodes = I.shape[1]
+	for w in layers:
+		W.append( np.array( [ [ ( random()-0.5)*0.2 for x in range(w) ] for x in range(inputNodes) ] ) )
+		inputNodes = w
 
-		A = predict(I, H)
-
-		e = T-A[-1]
-		print "\nerror : ", e
-		for i in range(len(H)):
-			input = A[-(i+2)] if i+1<len(H) else I
-			a = A[-(i+1)]
-			h = H[-(i+1)]
-			e = e
-
-			print "\n\n", "e : ", e, "\n a : ", a, "\n h : ", h, "\ninput :", input
-			print "\nlr : ", LR*np.dot( np.transpose(input), e )
-
-			H[-(i+1)] = h+LR*np.dot( np.transpose(input), e )
-			
-			print "\n h after : ", H[-(i+1)]
-			e = np.dot( e, np.transpose(h) )
+	W = train( I, T, W )
 		
-		
-		print "\n\n"
-	
-	print predict( I, H )[-1]
-		
+	print predict( I, W )[-1]
