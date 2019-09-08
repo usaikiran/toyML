1342f2b36ebfbc2517935eb9b344a89ae48bb541
botsk <usaikiran89@gmail.com>
2019-09-08 11:54:24 +0530

    finishedrbf

--- algo/rbf.py
+++ algo/rbf.py
@@ -19,11 +19,11 @@ class RBF:
         self.targetVector = targetVector
 
 
-    def _calcDistance( inputVector, weights, nRBF ):
+    def _calcDistance( self, inputVector, weights, nRBF ):
 
         out = np.zeros( ( inputVector.shape[0], weights.shape[1] ) )
         for i in range(nRBF):
-            out[:,i] = self.exp( -np.sum( math.pow( self.inputVector-np.ones((1,inputVector.shape[0])*self.weights1), 2 ) )/( 2*(SIGMA**2) ) )
+            out[:,i] = np.exp( -np.sum( ( inputVector-np.ones( ( 1, inputVector.shape[1] ) )*weights[:,i] )**2, axis=1 )/( 2*(SIGMA**2) ) )
 
         return out
 
@@ -31,18 +31,19 @@ class RBF:
     def train( self, inputVector ):
 
         self.inputVector = inputVector
-        indices = np.random.shuffle(range(inputVector.shape))
+        indices = np.random.shuffle(range(inputVector.shape[0]))
 
-        for i in range(nRBF):
+        self.weights1 = np.zeros( (self.inputVector.shape[1], self.nRBF) )
+        for i in range(self.nRBF):
             self.weights1[:,i] = self.inputVector[i,:]
 
-        self.hidden = _calcDistance( inputVector, weights, nRBF )
+        self.hidden = self._calcDistance( self.inputVector, self.weights1, self.nRBF )
         self.weights2 = np.dot( np.linalg.pinv(self.hidden), self.targetVector )
 
 
     def predict( self, inputVector ):
 
-        hiddenOutput = _calcDistance( inputVector, weights, nRBF )
+        hiddenOutput = self._calcDistance( inputVector, self.weights1, self.nRBF )
         out = np.dot( hiddenOutput, self.weights2 )
 
         return out
@@ -50,4 +51,9 @@ class RBF:
 
 if __name__ == "__main__":
     
-    
+    I = np.array( [ [0, 0], [0, 1], [1, 0], [1, 1] ] )
+    T = np.array( [ [1], [0], [1], [0] ] )
+
+    rbf = RBF(3, T)
+    rbf.train(I)
+    print rbf.predict(I)
